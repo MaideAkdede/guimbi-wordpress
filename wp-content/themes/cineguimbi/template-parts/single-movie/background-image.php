1ee6d8bbd0428b22536d17ea288396f9ffd562f5
Maïde <maide.akdede@hotmail.com>
2022-03-09 16:31:47 +0100

    :pencil2: single-movie background image

---
+++ wp-content/themes/cineguimbi/template-parts/single-movie/background-image.php
@@ -0,0 +1,5 @@
+<div class="w-screen h-4/5 lg:h-single-film-bg absolute -z-3 top-0 overflow-hidden" aria-hidden="true">
+    <img class="w-full h-full object-cover object-center blur brightness-50 scale-105"
+         src="<?php echo get_template_directory_uri() . '/resources/images/movie/bg-01.png' ?>"
+         alt="Adieu Monsieur Haffman">
+</div>
