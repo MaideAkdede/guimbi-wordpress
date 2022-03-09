5befc7ce30ff84f81d5c073682bbaf55a47fa39b
Maïde <maide.akdede@hotmail.com>
2022-03-09 16:30:43 +0100

    :tada: Template pAGE : All movie

---
+++ wp-content/themes/cineguimbi/films.php
@@ -0,0 +1,208 @@
+<?php /* Template Name: Tous les Films */ ?>
+<?php get_header(); ?>
+<h1 class="underline-title">Tous les films</h1>
+
+<div class="bg-beige">
+    <div class="sort relative">
+        <a title="Affiner la sélection"
+           href=""
+           class="lg:hidden button bg-beige border-b border-primary/40 group block text-primary text-center text-sm font-black font-family-black uppercase tracking-widest px-7 py-5 flex items-center justify-between duration-150 hover:bg-primary/40">
+            Affiner la sélection
+            <span class="bg-primary rounded-full grid place-content-center w-12.5 aspect-square duration-150 fill-white group-hover:rotate-12 group-hover:fill-primary group-hover:bg-white">
+            <svg xmlns="http://www.w3.org/2000/svg" width="8.641" height="29.524" viewBox="0 0 8.641 29.524">
+                <g id="Groupe_194" data-name="Groupe 194" transform="translate(-8 -0.085)">
+                <g id="Groupe_172" data-name="Groupe 172" transform="translate(8 0.085)">
+                <g id="Groupe_173" data-name="Groupe 173" transform="translate(0)">
+                <path id="Ligne_88" data-name="Ligne 88"
+                      d="M-7.28,21.524A.72.72,0,0,1-8,20.8V-7.28A.72.72,0,0,1-7.28-8a.72.72,0,0,1,.72.72V20.8A.72.72,0,0,1-7.28,21.524Z"
+                      transform="translate(11.6 8)"/>
+                <path id="Tracé_47" data-name="Tracé 47"
+                      d="M4.321,277.041a.718.718,0,0,1-.509-.211l-3.6-3.6a.72.72,0,1,1,1.018-1.018L4.321,275.3l3.091-3.091a.72.72,0,1,1,1.018,1.018l-3.6,3.6A.718.718,0,0,1,4.321,277.041Z"
+                      transform="translate(0 -247.517)"/>
+                </g>
+                </g>
+                </g>
+            </svg>
+        </span>
+        </a>
+        <ul class="content hidden absolute lg:static lg:mx-auto top-22.75 bg-beige w-full lg:flex lg:max-w-xxl lg:justify-center lg:divide-primary/40 lg:divide-x lg:after:hidden after:block after:absolute after:w-full after:h-screen after:top-full after:bg-black/70 text-sm lg:text-xs xxl:text-sm z-1">
+            <li class="lg:px-5 lg:py-10 xxl:px-10">
+                <a href=""
+                   title="Trier par titre"
+                   class="bg-beige fill-primary text-primary lg:bg-primary lg:fill-white lg:text-white lg:border-none last:border-b-0 border-b border-primary/40 group block text-center font-black font-family-black uppercase tracking-widest px-7 py-5 flex items-center justify-between duration-150 hover:bg-primary/40 lg:p-0 lg:pr-4 lg:pt-1.5 lg:pb-1 xl:pr-6 lg:rounded-xl lg:hover:bg-primary/80 lg:hover:text-white">
+                    Par titre
+                    <span class="rounded-full grid place-content-center w-12.5 aspect-square duration-150 group-hover:bg-white lg:group-hover:bg-transparent lg:group-hover:fill-white lg:-order-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15.882" height="27.131" viewBox="0 0 15.882 27.131">
+            <g id="Groupe_170" data-name="Groupe 170" transform="translate(0 0)">
+            <path id="Tracé_47" data-name="Tracé 47"
+                  d="M110.615,9.264a.662.662,0,0,1-.636-.481l-2.037-7.175a.338.338,0,0,0-.3-.285.33.33,0,0,0-.3.268L105.3,8.783a.662.662,0,1,1-1.273-.362l2.037-7.175a1.621,1.621,0,0,1,3.158.017l2.033,7.158a.662.662,0,0,1-.637.843Z"
+                  transform="translate(-95.396 0)"/>
+            <path id="Tracé_48" data-name="Tracé 48"
+                  d="M118.2,65.323h-4.452a.662.662,0,1,1,0-1.323H118.2a.662.662,0,0,1,0,1.323Z"
+                  transform="translate(-103.733 -58.706)"/>
+            <path id="Tracé_49" data-name="Tracé 49"
+                  d="M117.956,225.264h-5.294A.662.662,0,0,1,112,224.6v-.794a.662.662,0,0,1,.153-.424l5.051-6.061h-3.881v.132a.662.662,0,0,1-1.323,0v-.794a.662.662,0,0,1,.662-.662h5.294a.662.662,0,0,1,.662.662v.794a.662.662,0,0,1-.153.424l-5.051,6.061h3.881v-.132a.662.662,0,1,1,1.323,0v.794A.662.662,0,0,1,117.956,225.264Z"
+                  transform="translate(-102.736 -198.133)"/>
+            <path id="Ligne_88" data-name="Ligne 88"
+                  d="M-7.338,19.131A.662.662,0,0,1-8,18.469V-7.338A.662.662,0,0,1-7.338-8a.662.662,0,0,1,.662.662V18.469A.662.662,0,0,1-7.338,19.131Z"
+                  transform="translate(11.309 8)"/>
+            <path id="Tracé_50" data-name="Tracé 50"
+                  d="M3.97,276.632a.66.66,0,0,1-.468-.194L.194,273.13a.662.662,0,0,1,.936-.936l2.841,2.841,2.841-2.841a.662.662,0,1,1,.936.936l-3.309,3.309A.66.66,0,0,1,3.97,276.632Z"
+                  transform="translate(0 -249.501)"/>
+            </g>
+            </svg>
+                </span>
+                </a></li>
+            <li class="lg:px-5 lg:py-10 xxl:px-10">
+                <a href=""
+                   title="Trier par date de sortie"
+                   class="bg-beige lg:border-none  last:border-b-0 border-b border-primary/40 group block text-primary text-center font-black font-family-black uppercase tracking-widest px-7 py-5 flex items-center justify-between duration-150 hover:bg-primary/40 lg:p-0 lg:pr-4 lg:pt-1.5 lg:pb-1 xl:pr-6 lg:rounded-xl lg:hover:bg-primary/80 lg:hover:text-white">
+                    Date de sortie
+                    <span class="rounded-full grid place-content-center w-12.5 aspect-square duration-150 group-hover:bg-white lg:group-hover:bg-transparent lg:group-hover:fill-white fill-primary lg:-order-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25.31" height="28.046" viewBox="0 0 25.31 28.046">
+            <g id="Groupe_199" data-name="Groupe 199" transform="translate(0 -0.085)">
+            <g id="Groupe_172" data-name="Groupe 172" transform="translate(0 0.085)">
+            <g id="Groupe_173" data-name="Groupe 173" transform="translate(0 0)">
+            <path id="Ligne_88" data-name="Ligne 88"
+                  d="M-7.316,20.046A.684.684,0,0,1-8,19.362V-7.316A.684.684,0,0,1-7.316-8a.684.684,0,0,1,.684.684V19.362A.684.684,0,0,1-7.316,20.046Z"
+                  transform="translate(11.42 8)"/>
+            <path id="Tracé_47" data-name="Tracé 47"
+                  d="M4.1,276.788a.682.682,0,0,1-.484-.2L.2,273.168a.684.684,0,1,1,.967-.967L4.1,275.137,7.041,272.2a.684.684,0,1,1,.967.967l-3.42,3.42A.682.682,0,0,1,4.1,276.788Z"
+                  transform="translate(0 -248.742)"/>
+            <path id="Ligne_89" data-name="Ligne 89"
+                  d="M7.733-6.632H-7.316A.684.684,0,0,1-8-7.316.684.684,0,0,1-7.316-8H7.733a.684.684,0,0,1,.684.684A.684.684,0,0,1,7.733-6.632Z"
+                  transform="translate(16.893 12.104)"/>
+            </g>
+            </g>
+            <path id="Ligne_90" data-name="Ligne 90"
+                  d="M5-6.632H-7.316A.684.684,0,0,1-8-7.316.684.684,0,0,1-7.316-8H5a.684.684,0,0,1,.684.684A.684.684,0,0,1,5-6.632Z"
+                  transform="translate(16.893 16.293)"/>
+            <path id="Ligne_91" data-name="Ligne 91"
+                  d="M2.261-6.632H-7.316A.684.684,0,0,1-8-7.316.684.684,0,0,1-7.316-8H2.261a.684.684,0,0,1,.684.684A.684.684,0,0,1,2.261-6.632Z"
+                  transform="translate(16.893 20.398)"/>
+            <path id="Ligne_92" data-name="Ligne 92"
+                  d="M-.475-6.632h-6.84A.684.684,0,0,1-8-7.316.684.684,0,0,1-7.316-8h6.84a.684.684,0,0,1,.684.684A.684.684,0,0,1-.475-6.632Z"
+                  transform="translate(16.893 24.502)"/>
+            <path id="Ligne_93" data-name="Ligne 93"
+                  d="M-3.212-6.632h-4.1A.684.684,0,0,1-8-7.316.684.684,0,0,1-7.316-8h4.1a.684.684,0,0,1,.684.684A.684.684,0,0,1-3.212-6.632Z"
+                  transform="translate(16.893 28.606)"/>
+            </g>
+            </svg>
+        </span>
+                </a>
+            </li>
+            <li class="lg:px-5 lg:py-10 xxl:px-10">
+                <a href=""
+                   title="En famille"
+                   class="bg-beige fill-primary lg:border-none last:border-b-0 border-b border-primary/40 group block text-primary text-center font-black font-family-black uppercase tracking-widest px-7 py-5 flex items-center justify-between duration-150 hover:bg-primary/40 lg:p-0 lg:pr-4 lg:pt-1.5 lg:pb-1 xl:pr-6 lg:rounded-xl lg:hover:bg-primary/80 lg:hover:text-white">
+                    En famille
+                    <span class="rounded-full grid place-content-center w-12.5 aspect-square duration-150 group-hover:bg-white lg:group-hover:bg-transparent lg:group-hover:fill-white lg:group-hover:stroke-white stroke-primary lg:-order-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20.177" height="28.742" viewBox="0 0 20.177 28.742">
+                <g id="Groupe_175" data-name="Groupe 175" transform="translate(0.268 0.249)">
+                <g id="Groupe_176" data-name="Groupe 176" transform="translate(0 0.001)">
+                <path id="Tracé_47" data-name="Tracé 47"
+                      d="M5.655,28.243H4.824V20.455h.832Zm1.718-.031c-.119-1.736-.2-3.585-.28-5.374-.076-1.725-.155-3.507-.267-5.183-.518-.539-1.114-1.246-1.692-1.93-.6-.713-1.225-1.451-1.736-1.971l.593-.582c.532.542,1.165,1.292,1.777,2.017s1.237,1.465,1.757,1.995l.108.11.01.153c.118,1.73.2,3.573.279,5.355s.16,3.627.279,5.353Zm4.553,0-.829-.057c.118-1.722.2-3.559.273-5.336s.155-3.633.275-5.372l.011-.152.108-.11c.52-.53,1.149-1.275,1.758-2s1.246-1.475,1.777-2.017l.593.582c-.51.52-1.133,1.257-1.735,1.97-.579.685-1.175,1.392-1.692,1.931-.113,1.684-.189,3.47-.264,5.2-.076,1.782-.155,3.625-.274,5.357m-8.622,0-.455-6.9-2.11,0,1.749-8.989L2.024,9.98l.816-.161.5,2.506-.016.08L1.748,20.482l1.879,0,.506,7.678ZM17.167,28.2l-.83-.042.495-9.8V9.9h.832v8.482Zm-7.106-.014H9.229V21.969h.832Zm4.606-.007h-.832v-9.64h.832ZM.829,18.445,0,18.384.709,8.8c.136-1.834,2.36-2.67,4.368-2.67h0c2.013,0,4.24.835,4.377,2.67l.064.868-.829.062-.064-.868c-.093-1.248-1.855-1.9-3.548-1.9h0c-1.689,0-3.447.654-3.539,1.9Zm18-.033-.1-9.574c-.013-1.219-2.313-1.88-4.47-1.884a11.979,11.979,0,0,0-4.691.767l-.306-.773a12.794,12.794,0,0,1,5-.825c2.62,0,5.276.843,5.3,2.706l.1,9.574ZM9.589,15.566a8.469,8.469,0,0,1-2.054-.144L4.21,12.828l.511-.655,3.225,2.516c.082.01.213.018.378.025a1.857,1.857,0,0,1-.454-.529,5,5,0,0,1-.33-2.14,1.928,1.928,0,0,1,2.11-1.658,1.93,1.93,0,0,1,2.112,1.658,5.024,5.024,0,0,1-.33,2.14,1.865,1.865,0,0,1-.453.529c.161-.006.289-.015.369-.024l3.223-2.516.511.655-3.3,2.575a8.727,8.727,0,0,1-2.192.162M8.366,12.122a4.379,4.379,0,0,0,.23,1.66,1.425,1.425,0,0,0,.875.6.961.961,0,0,0,.391-.006,1.416,1.416,0,0,0,.843-.591,4.394,4.394,0,0,0,.229-1.66c-.063-.667-.705-.9-1.285-.9s-1.221.238-1.284.905M14.245,5.7a2.5,2.5,0,0,1-.381-.036,2.6,2.6,0,0,1-1.674-1.18,6.04,6.04,0,0,1-.385-2.541A2.281,2.281,0,0,1,14.251,0,2.283,2.283,0,0,1,16.7,1.939a6.025,6.025,0,0,1-.386,2.542,2.618,2.618,0,0,1-1.64,1.174,2.832,2.832,0,0,1-.426.042M14.251.832a1.463,1.463,0,0,0-1.617,1.184,5.449,5.449,0,0,0,.285,2.065,1.768,1.768,0,0,0,1.1.764,1.226,1.226,0,0,0,.5-.006,1.773,1.773,0,0,0,1.067-.757,5.419,5.419,0,0,0,.287-2.066A1.464,1.464,0,0,0,14.251.832M5.079,5.7A2.5,2.5,0,0,1,4.7,5.661a2.6,2.6,0,0,1-1.674-1.18,6.038,6.038,0,0,1-.385-2.541A2.281,2.281,0,0,1,5.085,0,2.283,2.283,0,0,1,7.531,1.939h0a6.06,6.06,0,0,1-.385,2.541A2.622,2.622,0,0,1,5.505,5.655a2.832,2.832,0,0,1-.426.042M5.085.832A1.463,1.463,0,0,0,3.467,2.016a5.431,5.431,0,0,0,.286,2.065,1.793,1.793,0,0,0,1.1.765,1.226,1.226,0,0,0,.5-.006,1.773,1.773,0,0,0,1.067-.758A5.434,5.434,0,0,0,6.7,2.015,1.464,1.464,0,0,0,5.085.832"
+                      transform="translate(0 -0.001)" stroke-width="0.5"/>
+                </g>
+                </g>
+                </svg>
+            </span>
+                </a>
+            </li>
+            <li class="lg:px-5 lg:py-10 xxl:px-10">
+                <a href=""
+                   title="Trier par date de sortie"
+                   class="bg-beige fill-primary lg:border-none last:border-b-0 border-b border-primary/40 group block text-primary text-center font-black font-family-black uppercase tracking-widest px-7 py-5 flex items-center justify-between duration-150 hover:bg-primary/40 lg:p-0 lg:pr-4 lg:pt-1.5 lg:pb-1 xl:pr-6 lg:rounded-xl lg:hover:bg-primary/80 lg:hover:text-white">
+                    <span>Par genre <i class="ml-2.5 fa-solid fa-angle-down"></i></span>
+                    <span class="rounded-full grid place-content-center w-12.5 aspect-square duration-150 group-hover:bg-white lg:group-hover:bg-transparent lg:group-hover:fill-white lg:-order-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="19.917" height="30.84" viewBox="0 0 19.917 30.84">
+                <g id="Groupe_178" data-name="Groupe 178" transform="translate(-0.069 0.1)">
+                <g id="Groupe_179" data-name="Groupe 179" transform="translate(0.069 -0.1)">
+                <path id="Tracé_48" data-name="Tracé 48"
+                      d="M8.352,166.954a.643.643,0,0,1-.642-.643v-7.427l-7.5-6.876A.643.643,0,0,1,0,151.534v-2.891a.642.642,0,1,1,1.285,0v2.609l7.5,6.876a.642.642,0,0,1,.208.474v6.159l1.927-1.927V158.6a.642.642,0,0,1,.208-.474l7.5-6.876v-2.609a.642.642,0,1,1,1.285,0v2.891a.643.643,0,0,1-.208.474l-7.5,6.876V163.1a.642.642,0,0,1-.188.454l-3.212,3.212A.642.642,0,0,1,8.352,166.954Z"
+                      transform="translate(0 -136.114)"/>
+                <path id="Tracé_49" data-name="Tracé 49"
+                      d="M9.959,150.891c-.407,0-9.959-.026-9.959-2.249a.643.643,0,0,1,1.261-.176c.615.468,4.256,1.14,8.7,1.14s8.083-.671,8.7-1.14a.643.643,0,0,1,1.261.176C19.917,150.866,10.365,150.891,9.959,150.891Z"
+                      transform="translate(0 -136.114)"/>
+                <path id="Tracé_50" data-name="Tracé 50"
+                      d="M183.328,134.182a.643.643,0,0,1-.617-.463,10.659,10.659,0,0,0-3.765-.86.642.642,0,1,1,.151-1.276c4.873.576,4.873,1.509,4.873,1.957A.642.642,0,0,1,183.328,134.182Z"
+                      transform="translate(-164.053 -121.011)"/>
+                <path id="Tracé_51" data-name="Tracé 51"
+                      d="M.642,140.475A.642.642,0,0,1,0,139.833c0-.786.933-1.154,1.745-1.387a.642.642,0,0,1,.354,1.235,3.073,3.073,0,0,0-.84.333A.643.643,0,0,1,.642,140.475Z"
+                      transform="translate(0 -127.304)"/>
+                <path id="Tracé_52" data-name="Tracé 52"
+                      d="M41.927,70.425H40.642A.642.642,0,0,1,40,69.782v-5.14A.643.643,0,0,1,40.642,64h5.14a.643.643,0,0,1,.642.643V66.57a.642.642,0,0,1-1.285,0V65.285H41.285V69.14h.642a.642.642,0,0,1,0,1.285Z"
+                      transform="translate(-36.788 -58.86)"/>
+                <path id="Rectangle_100" data-name="Rectangle 100"
+                      d="M-7.358-8H-.933a.642.642,0,0,1,.642.642V-.933A.642.642,0,0,1-.933-.29H-7.358A.642.642,0,0,1-8-.933V-7.358A.642.642,0,0,1-7.358-8Zm5.782,1.285h-5.14v5.14h5.14Z"
+                      transform="translate(18.922 8)"/>
+                <path id="Tracé_53" data-name="Tracé 53"
+                      d="M86.425,115.855a.642.642,0,0,1-.642-.642v-1.927h-4.5v1.927a.642.642,0,0,1-1.285,0v-2.57a.642.642,0,0,1,.642-.642h5.782a.643.643,0,0,1,.643.642v2.57A.643.643,0,0,1,86.425,115.855Z"
+                      transform="translate(-73.575 -103.005)"/>
+                </g>
+                </g>
+                </svg>
+            </span>
+                </a>
+            </li>
+            <li class="lg:px-5 lg:py-10 xxl:px-10">
+                <form action="" class="text-base text-primary bg-beige px-3 py-4 relative lg:p-0">
+                    <label for="sort-search" class="sr-only">Afficher recherche</label>
+                    <input class="py-4 block w-full pl-14 placeholder:text-primary/60 xl:w-42ch" type="text"
+                           id="sort-search"
+                           name="sort-search" placeholder="Rechercher un film">
+                    <button type="submit" class="absolute top-1/2 -translate-y-1/2 left-5 lg:left-2.5">
+                        <span class="sr-only">Envoyer</span>
+                        <i class="fa-regular fa-magnifying-glass text-xl px-2.5"></i>
+                    </button>
+                </form>
+            </li>
+        </ul>
+        <div class="bg-beige px-5 py-10 lg:pt-0 lg:pb-44 xl:grid xl:grid-cols-2 gap-10 max-w-xxl mx-auto">
+            <?php for ($x = 1; $x <= 5; $x++): ?>
+                <article
+                        class="group bg-white first:mt-0 last:mb-0 mt-10 mb-10 xl:my-0 max-w-md shadow-md mx-auto md:max-w-none md:grid md:grid-cols-movie-cast relative">
+                    <img class="w-full h-full object-cover"
+                         src="<?php echo get_template_directory_uri() . '/resources/images/movie/' . $x . '.png' ?>"
+                         alt="">
+                    <div class="px-5 py-10 md:grid md:place-content-center">
+                        <h2 class="text-lg font-bold font-family-bold">À plein temps</h2>
+                        <p class="text-primary text-15px font-bold font-family-bold"> Sortie :
+                            <time>23 mars 2022</time>
+                        </p>
+                        <div class="flex mt-3 mb-5">
+                            <img title="Peur et angoisse"
+                                 class="aspect-square w-6.5"
+                                 src="<?php echo get_template_directory_uri() . '/resources/images/age/peur.png' ?>"
+                                 alt="Peur et angoisse">
+                            <img title="Potentiellement préjudiciable aux enfants de moins de 9 ans"
+                                 class="aspect-square w-6.5"
+                                 src="<?php echo get_template_directory_uri() . '/resources/images/age/age9.png' ?>"
+                                 alt="Potentiellement préjudiciable aux enfants de moins de 9 ans">
+                            <img title="Langage grossier"
+                                 class="aspect-square w-6.5"
+                                 src="<?php echo get_template_directory_uri() . '/resources/images/age/langage.png' ?>"
+                                 alt="Langage Grossier">
+                            <img title="Tout public"
+                                 class="aspect-square w-6.5"
+                                 src="<?php echo get_template_directory_uri() . '/resources/images/age/al.png' ?>"
+                                 alt="Tout public"">
+                        </div>
+                        <p class="line-clamp-6 font-light font-family-light">Lorem ipsum dolor sit amet, consectetur
+                            adipisicing elit. Animi ducimus
+                            ea
+                            incidunt ipsam iste itaque laborum magni maxime nam nemo odit officia officiis perferendis,
+                            praesentium quas sint sunt ullam vel Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit.
+                            At
+                            debitis illum libero nihil optio ratione saepe, sequi veniam. Amet at beatae dignissimos
+                            dolorum
+                            libero nemo ratione tempora vel veniam voluptates.</p>
+                        <p class="block max-w-max font-bold font-family-bold uppercase text-primary text-xs border border-primary px-5 pt-3.5 pb-2.5 group-hover:text-white shadow-set-hover group-hover:shadow-hover duration-150 ease-in tracking-widest mt-5">Voir la fiche du film</p></div>
+                    <a href="/adieu-monsieur-haffmann" title="Voir la fiche du film XXX" class="absolute block h-full w-full top-0 left-0 group-hover:border-2px group-hover:border-primary focus:outline-none focus:border-2px focus:border-primary"></a>
+                </article>
+            <?php endfor ?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
