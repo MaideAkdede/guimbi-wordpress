f4760e761e66696c36615bcc227af437a1ca1547
Maïde <maide.akdede@hotmail.com>
2022-03-09 16:29:47 +0100

    :pencil2: update classic templage page design

--- wp-content/themes/cineguimbi/index.php
+++ wp-content/themes/cineguimbi/index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
 <!-- FRONT PAGE -->
 <?php if (is_front_page()) : ?>
     <?php get_template_part('template-parts/hero'); ?>
@@ -11,4 +10,15 @@
     <?php get_template_part('template-parts/home/support'); ?>
 <?php endif; ?>
 <!-- END FRONT PAGE -->
+
+<!-- CLASSIC PAGE -->
+<?php if ( have_posts() ) : ?>
+    <?php
+    while ( have_posts() ) :
+        the_post();
+        ?>
+        <?php get_template_part( 'template-parts/content', 'single' ); ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+<!-- END CLASSIC PAGE -->
 <?php get_footer(); ?>
--- wp-content/themes/cineguimbi/template-parts/content-single.php
+++ wp-content/themes/cineguimbi/template-parts/content-single.php
@@ -1,11 +1,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header mb-4">
-		<?php the_title( sprintf( '<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
-	</header>
+		<?php the_title( sprintf( '<h1 class="underline-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-	<div class="entry-content">
+
+	<div class="wysiwyg max-w-wysiwyg mx-auto px-5 pb-12 lg:pb-24">
 		<?php the_content(); ?>
 
 		<?php
